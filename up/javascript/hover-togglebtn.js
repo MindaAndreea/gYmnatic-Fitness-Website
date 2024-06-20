3c4ae265e6845f5c06d835b9cc1bf193cd8211f4
MindaAndreea <165321457+MindaAndreea@users.noreply.github.com>
2024-06-21 00:18:36 +0300

    Header Styling

---
+++ up/javascript/hover-togglebtn.js
@@ -0,0 +1,9 @@
+function showTooltip(element) {
+  const tooltip = element.querySelector(".tooltip-text");
+  tooltip.style.display = "block";
+}
+
+function hideTooltip(element) {
+  const tooltip = element.querySelector(".tooltip-text");
+  tooltip.style.display = "none";
+}
